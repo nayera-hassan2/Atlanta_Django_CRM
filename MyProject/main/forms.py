babdbe10b57a996c79fc7910f605fe995fd2e253
nayera-hassan2 <nayerahassan80@gmail.com>
2024-01-10 13:20:05 +0530

    changes in css, forms, index, update_customer file

--- MyProject/main/forms.py
+++ MyProject/main/forms.py
@@ -6,4 +6,4 @@ from .models import CustomersRecords
 class CustomerUpdateForm(forms.ModelForm):
     class Meta:
         model = CustomersRecords
-        fields = ['first_name', 'last_name', 'email', 'phone_number', 'address', 'city', 'state', 'zipcode']
+        fields = '__all__'
